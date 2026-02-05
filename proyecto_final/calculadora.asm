740106b82b5073aaabb13216c2209290bdda39ab
superheroes6 <santibox888@gmail.com>
2026-02-05 12:55:49 +0100

    Issue #2: Implementar rutina read_input

--- proyecto_final/calculadora.asm
+++ proyecto_final/calculadora.asm
@@ -1,6 +1,15 @@
+section .bss
+buffer resb 64
+
 section .text
 global _start
     _start:
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, buffer
+    mov edx, 64
+    int 0x80
+
     mov eax, 1
     mov ebx, 0
     int 0x80
