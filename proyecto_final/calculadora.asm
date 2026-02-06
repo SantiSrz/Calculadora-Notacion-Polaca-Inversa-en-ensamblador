e9bf56e1bee4876af44fa3d3eca677e398263336
superheroes6 <santibox888@gmail.com>
2026-02-06 11:29:01 +0100

    Issue #3: Implementar rutina print_string

--- proyecto_final/calculadora.asm
+++ proyecto_final/calculadora.asm
@@ -1,15 +1,38 @@
+section .data
+mensaje db 'Escribe algo: ', 0xa
+longitud equ $ - mensaje
+
 section .bss
 buffer resb 64
 
 section .text
 global _start
     _start:
-    mov eax, 3
-    mov ebx, 0
+
+    mov ecx, mensaje
+    mov edx, longitud
+    call imprimir_texto
+
     mov ecx, buffer
     mov edx, 64
-    int 0x80
+    call leer_texto
+
+    mov ecx, buffer
+    mov edx, eax
+    call imprimir_texto
 
     mov eax, 1
     mov ebx, 0
     int 0x80
+
+imprimir_texto:
+    mov eax, 4
+    mov ebx, 1
+    int 0x80
+    ret
+
+leer_texto:
+    mov eax, 3
+    mov ebx, 0
+    int 0x80
+    ret
