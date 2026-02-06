c35836e4b38e885ebb967e577f5882179794c969
superheroes6 <santibox888@gmail.com>
2026-02-06 19:38:58 +0100

    Issue #5: Implementar función ITOA (Integer to ASCII)

--- proyecto_final/calculadora.asm
+++ proyecto_final/calculadora.asm
@@ -1,14 +1,14 @@
 section .data
-mensaje db 'Escribe el numero: ', 0xa
-longitud equ $ - mensaje
+    mensaje db 'Escribe el numero: ', 0xa
+    longitud equ $ - mensaje
 
 section .bss
-buffer resb 64
+    buffer resb 64
 
 section .text
 global _start
-    _start:
 
+_start:
     mov ecx, mensaje
     mov edx, longitud
     call imprimir_texto
@@ -18,11 +18,12 @@ global _start
     call leer_texto
 
     mov esi, buffer
-    call ATOII
-    mov ebx, eax
+    call ATOI
+
+    call ITOA
 
     mov eax, 1
-
+    mov ebx, 0
     int 0x80
 
 imprimir_texto:
@@ -37,22 +38,62 @@ leer_texto:
     int 0x80
     ret
 
-ATOII:
+ATOI:
     mov eax, 0
-    bucle:
-        mov ebx, 0
-        mov bl, [esi]
-        cmp bl, 0
-        je fin
-        cmp bl, 0xA
-        je fin
-        cmp bl, 0x20
-        je fin
-        sub bl, '0'
-        mov ecx, 10
-        mul ecx
-        add eax, ebx
-        inc esi
-        jmp bucle
-    fin:
-        ret
+    
+.bucle_atoi:
+    mov ebx, 0
+    mov bl, [esi]
+    
+    cmp bl, 0
+    je .fin_atoi
+    
+    cmp bl, 0xA
+    je .fin_atoi
+    
+    cmp bl, 0x20
+    je .fin_atoi
+    
+    sub bl, '0'
+    mov ecx, 10
+    mul ecx
+    add eax, ebx
+    inc esi
+    jmp .bucle_atoi
+
+.fin_atoi:
+    ret
+
+ITOA:
+    mov ecx, 0
+    mov ebx, 10
+
+.bucle_division:
+    mov edx, 0
+    div ebx
+    
+    add edx, '0'
+    push edx
+    inc ecx
+    
+    cmp eax, 0
+    jne .bucle_division
+
+    mov edi, buffer
+
+.bucle_print:
+    pop eax
+    mov [edi], al
+    inc edi
+    loop .bucle_print
+
+    mov byte [edi], 0xA
+    inc edi
+    
+    sub edi, buffer
+    
+    mov edx, edi
+    mov ecx, buffer
+    
+    call imprimir_texto
+    ret
