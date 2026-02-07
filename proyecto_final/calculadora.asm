390a27fead4ca6d6940830ba314327a9868ac79d
superheroes6 <santibox888@gmail.com>
2026-02-07 17:34:59 +0100

    Issue #6: Estructura de Pila Personalizada (Push/Pop)

--- proyecto_final/calculadora.asm
+++ proyecto_final/calculadora.asm
@@ -1,9 +1,11 @@
 section .data
-    mensaje db 'Escribe el numero: ', 0xa
+    mensaje db 'Debe salir 20 y luego 10 -> ', 0xa
     longitud equ $ - mensaje
 
 section .bss
     buffer resb 64
+    stack_data resd 256
+    stack_ptr resd 1
 
 section .text
 global _start
@@ -13,13 +15,16 @@ _start:
     mov edx, longitud
     call imprimir_texto
 
-    mov ecx, buffer
-    mov edx, 64
-    call leer_texto
+    mov eax, 10
+    call custom_push
 
-    mov esi, buffer
-    call ATOI
+    mov eax, 20
+    call custom_push
 
+    call custom_pop
+    call ITOA
+
+    call custom_pop
     call ITOA
 
     mov eax, 1
@@ -40,60 +45,59 @@ leer_texto:
 
 ATOI:
     mov eax, 0
-    
 .bucle_atoi:
     mov ebx, 0
     mov bl, [esi]
-    
     cmp bl, 0
     je .fin_atoi
-    
     cmp bl, 0xA
     je .fin_atoi
-    
     cmp bl, 0x20
     je .fin_atoi
-    
     sub bl, '0'
     mov ecx, 10
     mul ecx
     add eax, ebx
     inc esi
     jmp .bucle_atoi
-
 .fin_atoi:
     ret
 
 ITOA:
     mov ecx, 0
     mov ebx, 10
-
 .bucle_division:
     mov edx, 0
     div ebx
-    
     add edx, '0'
     push edx
     inc ecx
-    
     cmp eax, 0
     jne .bucle_division
-
     mov edi, buffer
-
 .bucle_print:
     pop eax
     mov [edi], al
     inc edi
     loop .bucle_print
-
     mov byte [edi], 0xA
     inc edi
-    
     sub edi, buffer
-    
     mov edx, edi
     mov ecx, buffer
-    
     call imprimir_texto
+    ret
+
+custom_push:
+    mov edi, [stack_ptr]
+    mov [stack_data + edi], eax
+    add edi, 4
+    mov [stack_ptr], edi
+    ret
+
+custom_pop:
+    mov edi, [stack_ptr]
+    sub edi, 4
+    mov eax, [stack_data + edi]
+    mov [stack_ptr], edi
     ret
