22c183468d7923fab43812c8f464d353a5fd031f
superheroes6 <santibox888@gmail.com>
2026-02-24 11:41:37 +0100

    Issue #8: Implementar Suma y Resta

--- proyecto_final/calculadora.asm
+++ proyecto_final/calculadora.asm
@@ -29,6 +29,7 @@ _start:
     je fin
     cmp bl, 0x20
     je .saltar_espacio
+    
     cmp bl, '0'
     jl .es_operador
     cmp bl, '9'
@@ -43,86 +44,39 @@ _start:
     jmp .bucle_principal
 
 .es_operador:
+    cmp bl, '+'
+    je .hacer_suma
+    
+    cmp bl, '-'
+    je .hacer_resta
+
     inc esi
     jmp .bucle_principal
 
-imprimir_texto:
-    mov eax, 4
-    mov ebx, 1
-    int 0x80
-    ret
-
-leer_texto:
-    mov eax, 3
-    mov ebx, 0
-    int 0x80
-    ret
-
-ATOI:
-    mov eax, 0
-.bucle_atoi:
-    mov ebx, 0
-    mov bl, [esi]
-    cmp bl, 0
-    je .fin_atoi
-    cmp bl, 0xA
-    je .fin_atoi
-    cmp bl, 0x20
-    je .fin_atoi
-    sub bl, '0'
-    mov ecx, 10
-    mul ecx
+.hacer_suma:
+    call custom_pop
+    mov ebx, eax  
+    
+    call custom_pop
     add eax, ebx
+    
+    call custom_push
     inc esi
-    jmp .bucle_atoi
-.fin_atoi:
-    ret
+    jmp .bucle_principal
 
-ITOA:
-    mov ecx, 0
-    mov ebx, 10
-.bucle_division:
-    mov edx, 0
-    div ebx
-    add edx, '0'
-    push edx
-    inc ecx
-    cmp eax, 0
-    jne .bucle_division
-    mov edi, buffer
-.bucle_print:
-    pop eax
-    mov [edi], al
-    inc edi
-    loop .bucle_print
-    mov byte [edi], 0xA
-    inc edi
-    sub edi, buffer
-    mov edx, edi
-    mov ecx, buffer
-    call imprimir_texto
-    ret
-
-custom_push:
-    mov edi, [stack_ptr]
-    mov [stack_data + edi], eax
-    add edi, 4
-    mov [stack_ptr], edi
-    ret
-
-custom_pop:
-    mov edi, [stack_ptr]
-    sub edi, 4
-    mov eax, [stack_data + edi]
-    mov [stack_ptr], edi
-    ret
+.hacer_resta:
+    call custom_pop
+    mov ebx, eax
+    
+    call custom_pop
+    sub eax, ebx
+    
+    call custom_push
+    inc esi 
+    jmp .bucle_principal
 
 fin:
-
-    call custom_pop
-    call ITOA
-
-    call custom_pop
+    call custom_pop 
     call ITOA
 
     mov eax, 1
